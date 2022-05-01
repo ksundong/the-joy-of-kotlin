9f368bb2241912a4f30b9468013bc6620d002094
Dion <ksundong@gmail.com>
2022-05-01 22:38:55 +0900

    feat: 부수효과를 프로그램의 외부로 이동시켜 부수효과 없는 프로그램을 만듦

--- src/main/kotlin/chapter1/SafetyProgramming.kt
+++ src/main/kotlin/chapter1/SafetyProgramming.kt
@@ -1,13 +1,26 @@
 package chapter1
 
-fun buyDonut(creditCard: CreditCard): Donut { // need to mock CreditCard
+/**
+ * 이 단계에서는 더 이상 신용카드 지급이 어떻게 이뤄지는지를 신경쓰지 않는다.
+ */
+fun buyDonut(creditCard: CreditCard): Purchase {
     val donut = Donut()
-    creditCard.charge(donut.price) // side-effect
-    return donut
+    val payment = Payment(creditCard, donut.price)
+    return Purchase(donut, payment)
 }
 
 class CreditCard {
-    fun charge(price: Double) {}
+    /**
+     * 이것이 이뤄지는지 신경쓰지 않음
+     *
+     * 지급을 즉시 처리할 수 있고, 나중에 처리하기 위해 저장할 수 있음
+     * 같은 카드에 대한 처리를 한 번에 수행할 수도 있음
+     */
+    fun charge(price: Int) {}
 }
 
-class Donut(val price: Double = 10.0)
+class Donut(val price: Int = 10)
+
+class Payment(val creditCard: CreditCard, val amount: Int)
+
+class Purchase(val donut: Donut, val payment: Payment)
